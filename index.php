4db6211ec88cbdd66564844680e28fa4d98013ed
Mountain <hsgood01@gmail.com>
2021-02-21 21:34:38 +0900

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <title>LOL Replay Helper</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <!-- 로그인 -->
+        <!-- 글목록 -->
+        
+            <!-- 글삭제 버튼으로 -->
+        <!-- 글쓰기 버튼으로 -->
+        <!-- 글삭제 버튼으로 -->
+        <!-- 글수정 버튼으로 -->
+        <!-- 마인드맵 수정하기 -->
+    </body>
+</html>
